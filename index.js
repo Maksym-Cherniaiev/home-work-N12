191f8665544885893aa9a490e700dc78beac705c
Maksym <likeran2@gmail.com>
2019-05-03 11:46:18 +0300

    iterator logic fixed

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 class Iterator {
-  constructor(userValue) {
-    this.userValue = userValue;
+  constructor() {
+    this.currentValue = 1;
+  }
+
+  rewind() {
+    this.currentValue = 1;
+    const fiboncciNum = new Fibonacci(this.currentValue);
+    return (`current index: ${fiboncciNum.index}, current number: ${fiboncciNum.number}`);
   }
   
   current() {
-    const fiboncciNum = new Fibonacci(this.userValue);
+    const fiboncciNum = new Fibonacci(this.currentValue);
     return (`current index: ${fiboncciNum.index}, current number: ${fiboncciNum.number}`);
   }
 
   next() {
-    const fiboncciNum = new Fibonacci(this.userValue + 1);
+    this.currentValue = this.currentValue + 1
+    const fiboncciNum = new Fibonacci(this.currentValue);
     return (`next index: ${fiboncciNum.index}, next number: ${fiboncciNum.number}`);
   }
 
   prev() {
-    const fiboncciNum = new Fibonacci(this.userValue - 1);
+    this.currentValue = this.currentValue - 1
+    const fiboncciNum = new Fibonacci(this.currentValue);
     return (`previous index: ${fiboncciNum.index}, previous number: ${fiboncciNum.number}`);
   }
 
   key() {
-    const fiboncciNum = new Fibonacci(this.userValue);
+    const fiboncciNum = new Fibonacci(this.currentValue);
     return (`current index: ${fiboncciNum.index}`);
   }
 }
 
-const calc = new Iterator(8);
+const calc = new Iterator();
 
 console.log(calc.current());
 console.log(calc.prev());
 console.log(calc.next());
 console.log(calc.key());
+console.log(calc.rewind());
